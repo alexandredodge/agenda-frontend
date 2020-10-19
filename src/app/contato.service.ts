79ec828743631a62ae3d938ecfc536936c9cc65e
Jose Alexandre Oliveira de Souza <jose.osouza@PMCG.IMTI>
2020-10-19 15:10:43 -0400

    alts

--- src/app/contato.service.ts
+++ src/app/contato.service.ts
@@ -10,12 +10,21 @@ import { environment } from '../environments/environment'
 export class ContatoService {
 
   url: string = environment.apiBaseUrl;
-
+  ur:string;
   constructor(
     private http: HttpClient) { }
 
   save(contato: Contato):Observable<Contato>{
     return this.http.post<Contato>(this.url,contato);
+  }
+
+  list():Observable<Contato[]>{
+	return this.http.get<any>(this.url);
+  }
+
+  favorite(contato:Contato):Observable<any>{
+	//console.log(`${this.url}/${contato.id}/${contato.favorito}`);
+	return this.http.patch(`${this.url}/${contato.id}/favorito`,null);
   }
     
 }
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -13,6 +13,7 @@ export class ContatoComponent implements OnInit {
 
   formulario: FormGroup;
   listaDecontatos: Contato[] = [];
+  colunas = ['id','nome','email','favorito'];
 
   constructor(
     private service:ContatoService,
@@ -20,13 +21,30 @@ export class ContatoComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-      this.formulario = this.fb.group({
-        nome: ['', Validators.required],
-        email:['',[Validators.email,Validators.required]]
-      })
+      this.configurarFormulario();
+	  this.listarContatos();
   }
 
-  testeValidacao(){
+  favoritar(contato: Contato){debugger
+	this.service.favorite(contato).subscribe( respostaRet =>{
+		contato.favorito = !contato.favorito;
+	})  
+  }
+
+  configurarFormulario(){
+	this.formulario = this.fb.group({
+        nome: ['', Validators.required],
+        email:['',[Validators.email,Validators.required]]
+	  })
+  }
+
+  listarContatos(){
+	  this.service.list().subscribe( retornoRet =>{
+		  this.listaDecontatos = retornoRet;
+	  } )
+  }
+
+  testeValidacao(){//SOMENTE TESTE
     console.log(this.formulario.value);
     const fValido = this.formulario.valid;
     if(fValido){
